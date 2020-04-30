7ce13d8441e8491a43c8a2a681d0d097dd561bbb
ian-jhon <ian@DESKTOP-3CH1K1E.localdomain>
2020-04-30 22:48:55 +0200

    Site updated: 2020-04-30 22:48:54

---
+++ dist/Mymusic_server.js
@@ -0,0 +1,10 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    id="2465890057"
+    server="netease"
+    type="playlist"
+    fixed: true,
+    volume="0.55"
+    autoplay: false,
+    
+});
